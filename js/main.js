3bed42f9f6d3f487fca2c6b4ee307a6a3467e638
lmProgramming <mikikubs@gmail.com>
2025-04-23 14:23:48 +0200

    feat: making my life easier

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+window.onload = () => {
+    "use strict";
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker
+            .register("./sw.js")
+            .then(() => console.log("Service Worker registered successfully."))
+            .catch((error) => console.error("Service Worker registration failed:", error));
+    }
+};
