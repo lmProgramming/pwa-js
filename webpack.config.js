7f520ba21711aa87722c5df7a826cdfa6268cc84
lmProgramming <mikikubs@gmail.com>
2025-04-29 11:56:58 +0200

    fix: update entry path in webpack configuration to point to the correct source directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './js/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
